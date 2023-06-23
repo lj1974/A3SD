1c17e7fca3ab6e710790bc2aed8f10066bf97c08
LJ <84136544+lj1974@users.noreply.github.com>
2023-06-23 20:56:26 -0300

    Delete CodigoEmProducao directory

--- CodigoEmProducao/Controller/login.py
+++
@@ -1,6 +0,0 @@
-from View.users import leitura_cpf
-
-
-def realizar_login():
-    result = leitura_cpf()
-    return '01.1', result
--- CodigoEmProducao/View/nome.py
+++
@@ -1,2 +0,0 @@
-def nome_usuarios(nome):
-   return print("\n\nBem-vindo, ", nome)
